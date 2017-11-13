03ea3d232f738d0b2cda8dd571ee1891e82c503c
Soo Jeong Lee <soo.leesj@gmail.com>
2017-11-12 20:53:11 -0800

    Update server.js
    Changed portnumber

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 3344);
 
 app.get('/',function(req,res){
   var qParams = [];
